035c7f1a978ac7dee94d130aae77b7cebaf1ec1f
Amin Rois Sinung Nugroho <sinunk1405@gmail.com>
2016-07-24 21:14:40 -0500

    Rewritten Chan's Algorithm.

--- convex_hull_implementations.hpp
+++ convex_hull_implementations.hpp
@@ -26,8 +26,8 @@ namespace csce {
 			algorithms.push_back(new csce::graham_scan_parallel<T>(thread_count));
 			algorithms.push_back(new csce::jarvis_march<T>(thread_count));
 			algorithms.push_back(new csce::jarvis_march_parallel<T>(thread_count));
-			//algorithms.push_back(new csce::chan_algo_parallel<T>(thread_count));
-			//algorithms.push_back(new csce::chan_algo<T>(thread_count));
+			algorithms.push_back(new csce::chan_algo_parallel<T, csce::graham_scan<T>, csce::jarvis_march<T>>(thread_count));
+			algorithms.push_back(new csce::chan_algo<T, csce::graham_scan<T>, csce::jarvis_march<T>>(thread_count));
 			algorithms.push_back(new csce::composable_hulls<T, csce::graham_scan<T>, csce::graham_scan_parallel<T>>(thread_count));
 			algorithms.push_back(new csce::composable_hulls<T, csce::graham_scan<T>, csce::jarvis_march_parallel<T>>(thread_count));
 			algorithms.push_back(new csce::composable_hulls<T, csce::jarvis_march<T>, csce::graham_scan_parallel<T>>(thread_count));
