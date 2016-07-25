eac2f6a8cd86cceac8120095ee8b8fb13b33ca68
Paul Martin <pmartin@casestack.com>
2016-07-24 23:21:48 -0500

    Chan's not compiling, so I have commented them out.

--- convex_hull_implementations.hpp
+++ convex_hull_implementations.hpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef convex_hull_implementations_hpp
 #define convex_hull_implementations_hpp
 
@@ -28,14 +27,14 @@ namespace csce {
 			algorithms.push_back(new csce::graham_scan_parallel<T>(thread_count));
 			algorithms.push_back(new csce::jarvis_march<T>(thread_count));
 			algorithms.push_back(new csce::jarvis_march_parallel<T>(thread_count));
-			algorithms.push_back(new csce::chan_algo_parallel<T>(thread_count));
-			algorithms.push_back(new csce::chan_algo<T>(thread_count));
+			//algorithms.push_back(new csce::chan_algo_parallel<T>(thread_count));
+			//algorithms.push_back(new csce::chan_algo<T>(thread_count));
 			algorithms.push_back(new csce::quick_hull<T>(thread_count));
 			algorithms.push_back(new csce::composable_hulls<T, csce::graham_scan_parallel<T>, csce::graham_scan_parallel<T>>(thread_count));
 			algorithms.push_back(new csce::composable_hulls<T, csce::graham_scan_parallel<T>, csce::jarvis_march_parallel<T>>(thread_count));
 			algorithms.push_back(new csce::composable_hulls<T, csce::jarvis_march_parallel<T>, csce::graham_scan_parallel<T>>(thread_count));
 			algorithms.push_back(new csce::composable_hulls<T, csce::jarvis_march_parallel<T>, csce::jarvis_march_parallel<T>>(thread_count));
-csce::quick_hull_parallel<T>(thread_count));
+			algorithms.push_back(new csce::quick_hull_parallel<T>(thread_count));
 			
 			return algorithms;
 		}
@@ -43,47 +42,3 @@ csce::quick_hull_parallel<T>(thread_count));
 }
 
 #endif /* convex_hull_implementations_hpp */
-=======
-#ifndef convex_hull_implementations_hpp
-#define convex_hull_implementations_hpp
-
-#include <vector>
-
-#include "convex_hull_base.hpp"
-#include "graham_scan.hpp"
-#include "jarvis_march.hpp"
-#include "graham_scan_parallel.hpp"
-#include "jarvis_march_parallel.hpp"
-#include "chan_algo.hpp"
-#include "chan_algo_parallel.hpp"
-#include "composable_hulls.hpp"
-#include "quick_hull.hpp"
-
-namespace csce {
-	namespace convex_hull_implementations {
-		/**
-		 * This defines the convex hull implementations to run. To add a new implementation,
-		 * add an entry to the vector returned by this method.
-		 */
-		template<typename T>
-		std::vector<csce::convex_hull_base<T>*> list(int thread_count) {
-			std::vector<csce::convex_hull_base<T>*> algorithms;
-			algorithms.push_back(new csce::graham_scan<T>(thread_count));
-			algorithms.push_back(new csce::graham_scan_parallel<T>(thread_count));
-			algorithms.push_back(new csce::jarvis_march<T>(thread_count));
-			algorithms.push_back(new csce::jarvis_march_parallel<T>(thread_count));
-			algorithms.push_back(new csce::chan_algo_parallel<T, csce::graham_scan<T>, csce::jarvis_march<T>>(thread_count));
-			algorithms.push_back(new csce::chan_algo<T, csce::graham_scan<T>, csce::jarvis_march<T>>(thread_count));
-			algorithms.push_back(new csce::composable_hulls<T, csce::graham_scan<T>, csce::graham_scan_parallel<T>>(thread_count));
-			algorithms.push_back(new csce::composable_hulls<T, csce::graham_scan<T>, csce::jarvis_march_parallel<T>>(thread_count));
-			algorithms.push_back(new csce::composable_hulls<T, csce::jarvis_march<T>, csce::graham_scan_parallel<T>>(thread_count));
-			algorithms.push_back(new csce::composable_hulls<T, csce::jarvis_march<T>, csce::jarvis_march_parallel<T>>(thread_count));
-			algorithms.push_back(new csce::quick_hull<T>(thread_count));
-
-			return algorithms;
-		}
-	}
-}
-
-#endif /* convex_hull_implementations_hpp */
->>>>>>> 035c7f1a978ac7dee94d130aae77b7cebaf1ec1f
