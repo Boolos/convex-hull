a47fddd2fc70972361ccb14d3b6a83b94a1cf28c
Alex Shipman <alex@MacBook-Pro-2.local>
2016-07-24 22:26:27 -0500

    Updating Graham Scan parallel.

--- graham_scan_parallel.hpp
+++ graham_scan_parallel.hpp
@@ -6,11 +6,13 @@
 #include <utility>
 #include <thread>
 #include <mutex>
+#include <functional>
 
 #include "convex_hull_base.hpp"
 #include "point.hpp"
 #include "vector2d.hpp"
 #include "sort.hpp"
+#include "graham_scan.hpp"
 
 namespace csce {
 	template<typename T>
@@ -25,42 +27,33 @@ namespace csce {
 		
 		
 		std::vector<csce::point<T>> compute_hull(std::vector<csce::point<T>>& points) {
-			if(points.size() <= 3){
-				return points;
+			if(points.size() <= 1600){
+				//if the input size is small enough, it is better to run the
+				//sequential version of this algorithm. It also handles some
+				//corner cases, such as sets of points that have three points or less.
+				csce::graham_scan<T> sequential(this->nthreads);
+				return sequential.compute_hull(points);
 			}
-			
+	
 			std::size_t index = this->pivot_index(points);
 			if(index != 0){
-				std::swap(points[0], points[index]);
+				std::swap(points.front(), points[index]);
 			}
 			
-			csce::sort<T> sorter(points[0], this->nthreads);
+			csce::sort<T> sorter(points.front(), this->nthreads);
 			sorter.sort_array(points.begin() + 1, points.end());
-			
-			std::vector<csce::point<T>> s;
-			s.reserve(points.size() + 1);
-			
-			s.push_back(points[points.size() - 1]);
-			s.push_back(points[0]);
-			s.push_back(points[1]);
-			
-			int x=2;
-			while(x < points.size()){
-				std::size_t y = s.size() - 1;
-				csce::vector2d<T> a(s[y-1], s[y]);
-				csce::vector2d<T> b(s[y-1], points[x]);
-				if(a.ccw(b)){
-					s.push_back(points[x++]);
-				} else {
-					s.pop_back();
-				}
-			}
-			
-			if(s.front() == s.back()){
-                                s.pop_back(); //the polygon is closed, so remove the last element to remove the duplicate point
-                        }
+		
+			//the points are now sorted by angle with respect to the south-most point (points[0])
+			//so divide the points into four different quadrants and process each quadrant in a
+			//different thread.
+	
+			//the indices previously calculated are now invalidated after the sort
+			this->pivot_index(points);
 
-			return s;
+			//now npindex, epindex, spindex, wpindex represent the locations that the north,
+			//east, south, and west points respectively are located
+			
+			return convex_hull(points);
 		}
 		
 		
@@ -69,9 +62,59 @@ namespace csce {
 		
 	private:
 		
-		std::size_t pindex = 0;
+		std::size_t spindex = 0;
+		std::size_t npindex = 0;
+		std::size_t epindex = 0;
+		std::size_t wpindex = 0;
 		std::mutex pivot_mutex;
-		
+
+
+		void convex_hull_thread(const std::vector<csce::point<T>>& points, const std::size_t begin, const std::size_t end, std::vector<csce::point<T>>& output){
+			if(begin == end - 1){
+				output.push_back(points[begin]);
+				return;
+			}
+
+			output.push_back(begin == 0 ? points.back() : points[begin - 1]); //add the last point from the previous segment
+                        output.push_back(points[begin]);
+                        output.push_back(points[begin + 1]);
+
+                        std::size_t x = begin + 2;
+                        while(x < end){
+                                std::size_t y = output.size() - 1;
+                                csce::vector2d<T> a(output[y-1], output[y]);
+                                csce::vector2d<T> b(output[y-1], points[x]);
+                                if(a.ccw(b)){
+                                        output.push_back(points[x++]);
+                                } else {
+                                        output.pop_back();
+                                }
+                        }
+		}
+
+		std::vector<csce::point<T>> convex_hull(const std::vector<csce::point<T>>& points){
+			std::thread threads[4]; //there are four extreme points (N, E, S, W)
+			std::vector<std::vector<csce::point<T>>> convex_hulls(4);
+
+			threads[0] = std::thread(&csce::graham_scan_parallel<T>::convex_hull_thread, this, points, this->spindex, this->epindex, std::ref(convex_hulls[0]));
+			threads[1] = std::thread(&csce::graham_scan_parallel<T>::convex_hull_thread, this, points, this->epindex, this->npindex, std::ref(convex_hulls[1]));
+			threads[2] = std::thread(&csce::graham_scan_parallel<T>::convex_hull_thread, this, points, this->npindex, this->wpindex, std::ref(convex_hulls[2]));
+			threads[3] = std::thread(&csce::graham_scan_parallel<T>::convex_hull_thread, this, points, this->wpindex, points.size(), std::ref(convex_hulls[3]));
+
+			std::vector<csce::point<T>> output;
+                        for(int x=0; x<4; x++){
+                                threads[x].join();
+				for(auto& point : convex_hulls[x]){
+					output.push_back(point);
+				}
+                        }
+
+			//the output contains a mostly correct convex hull (which should be a lot smaller than the input array),
+			//so run the sequential algorithm to clean up an quadrant boundaries
+			csce::graham_scan<T> sequential(this->nthreads);
+                        return sequential.compute_hull(output);
+		}
+	
 		/**
 		 * Finds the index of the point with minimum y coordinate.
 		 * If there is a tie for minimum y coordinate, the index of the point with
@@ -93,39 +136,70 @@ namespace csce {
 			}
 			
 			delete[] threads;
-			return this->pindex;
+			return this->spindex;
 		}
 		
 		void pivot_index_thread(const std::vector<csce::point<T>>& points, int start, int stop) {
-			std::size_t index = start;
+			std::size_t sindex = start;
+			std::size_t nindex = start;
+			std::size_t eindex = start;
+			std::size_t windex = start;
 			for(std::size_t x=start; x<points.size() && x <= stop; x++){
-				const csce::point<T>& p0 = points[index];
-				const csce::point<T>& cur = points[x];
-				if(csce::math_utility::less_than(cur.y, p0.y)){
-					index = x;
-				} else if(csce::math_utility::equals(cur.y, p0.y)){
-					if(csce::math_utility::less_than(cur.x, p0.x)){
-						index = x;
-					}
-				}
+				this->get_pivot_points(points, x, nindex, eindex, sindex, windex);
 			}
-			this->update_pivot_index(points, index);
+			this->update_pivot_index(points, nindex, eindex, sindex, windex);
 		}
-		
-		void update_pivot_index(const std::vector<csce::point<T>>& points, const std::size_t& index) {
+	
+		void update_pivot_index(const std::vector<csce::point<T>>& points, const std::size_t& n, const std::size_t& e, const std::size_t& s, const std::size_t& w){
 			this->pivot_mutex.lock();
-			
-			const csce::point<T>& p0 = points[this->pindex];
-			const csce::point<T>& cur = points[index];
-			if(csce::math_utility::less_than(cur.y, p0.y)){
-				this->pindex = index;
-			} else if(csce::math_utility::equals(cur.y, p0.y)){
-				if(csce::math_utility::less_than(cur.x, p0.x)){
-					this->pindex = index;
-				}
-			}
-			
+			this->get_pivot_points(points, n, this->npindex, this->epindex, this->spindex, this->wpindex);
+			this->get_pivot_points(points, e, this->npindex, this->epindex, this->spindex, this->wpindex);
+			this->get_pivot_points(points, s, this->npindex, this->epindex, this->spindex, this->wpindex);
+			this->get_pivot_points(points, w, this->npindex, this->epindex, this->spindex, this->wpindex);
 			this->pivot_mutex.unlock();
+		}
+	
+		void get_pivot_points(const std::vector<csce::point<T>>& points, const std::size_t& x, std::size_t& n, std::size_t& e, std::size_t& s, std::size_t& w){
+			const csce::point<T>& ps = points[s];
+                        const csce::point<T>& pn = points[n];
+                        const csce::point<T>& pe = points[e];
+                        const csce::point<T>& pw = points[w];
+
+                        const csce::point<T>& cur = points[x];
+                        if(csce::math_utility::less_than(cur.y, ps.y)){
+                                s = x;
+                        } else if(csce::math_utility::equals(cur.y, ps.y)){
+                                if(csce::math_utility::less_than(cur.x, ps.x)){
+                                        s = x;
+                                }
+                        }
+                                
+
+                        if(csce::math_utility::less_than(pn.y, cur.y)){
+                                n = x;
+                        } else if(csce::math_utility::equals(cur.y, pn.y)){
+                                if(csce::math_utility::less_than(cur.x, pn.x)){
+                                        n = x;
+                                }
+                        }
+
+
+                        if(csce::math_utility::less_than(cur.x, pw.x)){
+                                w = x;
+                        } else if(csce::math_utility::equals(cur.x, pw.x)){
+                                if(csce::math_utility::less_than(cur.y, pw.y)){
+                                        w = x;
+                                }
+                        }
+
+
+                        if(csce::math_utility::less_than(pe.x, cur.x)){
+                                e = x;
+                        } else if(csce::math_utility::equals(cur.x, pe.x)){
+                                if(csce::math_utility::less_than(cur.y, pe.y)){
+                                        e = x;
+                                }
+                        }
 		}
 	};
 }
